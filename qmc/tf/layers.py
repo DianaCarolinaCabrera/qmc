a0b1e26adf33d3cf448f7cda90a02289cf49b6d3
Diana Carolina Cabrera Torres <69425802+DianaCarolinaCabrera@users.noreply.github.com>
2020-12-10 11:47:33 -0500

    commit master

--- qmc/tf/layers.py
+++ qmc/tf/layers.py
@@ -650,46 +650,47 @@ class DensityMatrixRegression(tf.keras.layers.Layer):
     def compute_output_shape(self, input_shape):
         return (input_shape[1], 2)
 
-# class BOWL1L2(tf.keras.layers.Layer):
-#     """BOWL1L2 layer for replace QFeatureMapRFF function.
-#     Represents the x_vectors after normalization L1, L2 and rebalanced:
+class BOWL1L2(tf.keras.layers.Layer):
+    """BOWL1L2 layer for replace QFeatureMapRFF function.
+    Represents the x_vectors after normalization L1, L2 and rebalanced:
     
-#     Input shape:
-#         (batch_size, dim_in)
-#         where dim_x is the dimension of the input state
-#     Output shape:
-#         (batch_size, dim)
-#     Arguments:
-#         input_dim: int. the dimension of the input
-#     """
-#     @typechecked
-#     def __init__(self, input_dim: int = 1000, **kwargs):
-#         super().__init__(**kwargs)
-#         if input_dim == 0:
-#             raise NotImplementedError("Dimension of input "
-#                                       "{}.".format(input_dim))
-#         self.input_dim = input_dim
-#         self.ctype = tf.Variable(initial_value = input_dim, 
-#                                 dtype = tf.float32,
-#                                 trainable = True,
-#                                 name = "Change_Type")
+    Input shape:
+        (batch_size, dim_in)
+        where dim_x is the dimension of the input state
+    Output shape:
+        (batch_size, dim)
+    Arguments:
+        input_dim: int. the dimension of the input
+    """
+    @typechecked
+    def __init__(self, input_dim: int = 1000, **kwargs):
+        super().__init__(**kwargs)
+        if input_dim == 0:
+            raise NotImplementedError("Dimension of input "
+                                      "{}.".format(input_dim))
+        self.input_dim = input_dim
+        # self.ctype = tf.Variable(initial_value = tf.zeros((input_dim,)), 
+        #                         dtype = tf.float32,
+        #                         trainable = False,
+        #                         name = "Change_Type")
 
-#     def call(self, inputs):
-#         psi = self.ctype(super().call(inputs))
-#         return psi
+    def call(self, inputs):
+        # self.ctype.assign_add(inputs)
+        return inputs
+
     
-#     def get_config(self):
-#         config = {
-#             "input_dim": self.input_dim
-#         }
-#         base_config = super().get_config()
-#         return {**base_config, **config}
+    def get_config(self):
+        config = {
+            "input_dim": self.input_dim
+        }
+        base_config = super().get_config()
+        return {**base_config, **config}
     
-#     def from_config(cls, config):
-#         return cls(**config)
+    def from_config(cls, config):
+        return cls(**config)
     
-#     def compute_output_shape(self, input_shape):
-#         return input_shape
+    def compute_output_shape(self, input_shape):
+        return input_shape
 
 # class BOWL1L2(tf.keras.layers.Layer):
 #     """Quantum feature map using random Fourier Features.
